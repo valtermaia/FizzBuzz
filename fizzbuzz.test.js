99d5d07dc5aea9afac8bf8f70cb3c41d37fe4299
Valter Maia <valtermaia@gmail.com>
2023-06-28 12:51:53 -0400

    espera que retorno fizz para múltiplos de 3

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -5,4 +5,10 @@ it("deve retornar um numero que não seja divisivel por 3 ou 5", () => {
     expect(fizzbuzz(1)).toBe(1);
     expect(fizzbuzz(2)).toBe(2);
     expect(fizzbuzz(4)).toBe(4);
-  });
+});
+
+it("deve retorna 'fizz' se o numero for divisivel por 3", () => {
+  expect(fizzbuzz(3)).toBe('fizz');
+  expect(fizzbuzz(6)).toBe('fizz');
+  expect(fizzbuzz(9)).toBe('fizz');
+});
