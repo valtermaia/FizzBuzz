defc9734643feeb63d45bd71039bcf473cf2d986
Valter Maia <valtermaia@gmail.com>
2023-06-28 12:54:22 -0400

    refatora classe fizzbuzz para retornar múltiplos de 3

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,4 +1,7 @@
 function fizzbuzz(n) {
+    if (n % 3 == 0) {
+        return 'fizz';
+    }
     return n;
 }
 
--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,13 +1,13 @@
 
 const fizzbuzz = require('./fizzbuzz');
 
-it("deve retornar um numero que não seja divisivel por 3 ou 5", () => {
+it("deve retornar um numero que não seja múltiplo de 3 ou 5", () => {
     expect(fizzbuzz(1)).toBe(1);
     expect(fizzbuzz(2)).toBe(2);
     expect(fizzbuzz(4)).toBe(4);
 });
 
-it("deve retorna 'fizz' se o numero for divisivel por 3", () => {
+it("deve retornar 'fizz' se o numero for múltiplo de 3", () => {
   expect(fizzbuzz(3)).toBe('fizz');
   expect(fizzbuzz(6)).toBe('fizz');
   expect(fizzbuzz(9)).toBe('fizz');
